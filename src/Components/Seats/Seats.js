32dbd138e5840703107cf198996c4a814bed06f4
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-11 22:42:17 -0300

    retirando console log

--- src/Components/Seats/Seats.js
+++ src/Components/Seats/Seats.js
@@ -4,7 +4,6 @@ import axios from "axios";
 
 export default function Seats(){
     const id = (useParams().idSessao);
-    console.log(id);
     useEffect(()=>{
         const promise= axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/showtimes/${id}/seats`);
         promise.then((answer)=>console.log(answer.data))
