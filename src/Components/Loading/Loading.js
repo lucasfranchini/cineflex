a5f4ef7c18486d74ee1bf1f98a454d8336806c1a
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-13 15:17:45 -0300

    personalizando loading

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,7 +1,12 @@
+import "./loading.css";
+import loading from "./loading.png";
+
+
 export default function Loading(){
     return (
-        <div>
-            <img src="https://lh3.googleusercontent.com/proxy/Bd0au_i6rLJFx2rEo-Qnvu52PhDdD53AOSquCLujYzJMQYo4-0zsr6NVGL175iGEBr7BqO4mkXPJkJZTvQjSzCOsRbapX8ye=s0-d" alt= "loading"/>
+        <div className="loading">
+            <img src={loading} alt= "loading"/>
+            <span>Carregando</span>
         </div>
     );
 }
